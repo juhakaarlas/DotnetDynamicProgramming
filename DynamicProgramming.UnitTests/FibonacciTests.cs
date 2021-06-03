1547cd819245ea6efd33cddb1379299618b36926
Juha Kaarlas <11892788+juhakaarlas@users.noreply.github.com>
2021-06-04 01:47:50 +0200

    Add project files.

---
+++ DynamicProgramming.UnitTests/FibonacciTests.cs
@@ -0,0 +1,44 @@
+using Fibonacci;
+using System;
+using Xunit;
+
+namespace DynamicProgramming.UnitTests
+{
+    public class FibonacciTests
+    {
+        [Fact]
+        public void BruteForceFibonacci_Returns_Base_Cases()
+        {
+            var testee = new BruteForceFibonacci();
+
+            Assert.Equal(1, testee.Fib(1));
+            Assert.Equal(1, testee.Fib(2));
+        }
+
+        [Fact]
+        public void BruteForceFibonacci_Handles_Zero()
+        {
+            var testee = new BruteForceFibonacci();
+            Assert.Equal(0, testee.Fib(0));
+        }
+
+        [Fact]
+        public void BruteForceFibonacci_Negatives_Throw_ArgumentException()
+        {
+            var testee = new BruteForceFibonacci();
+            Assert.Throws<ArgumentOutOfRangeException>( () => testee.Fib(-1));
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(3, 2)]
+        [InlineData(4, 3)]
+        [InlineData(5, 5)]
+        [InlineData(6, 8)]
+        public void BruteForceFibonacci_Calculates_Correctly(int n, int expected)
+        {
+            var testee = new BruteForceFibonacci();
+            Assert.Equal(expected, testee.Fib(n));
+        }
+    }
+}
