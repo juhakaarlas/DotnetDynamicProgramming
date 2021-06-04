631c0936e779d3a8c0f676057e99d093a1fd9db3
Juha Kaarlas <juha.kaarlas@gmail.com>
2021-06-04 14:05:22 +0200

    Memoized CanSum implementation with the first test cases.

---
+++ DynamicProgramming.UnitTests/CanSum/MemoizedCanSumTests.cs
@@ -0,0 +1,23 @@
+﻿using DynamicProgramming.CanSum;
+using Xunit;
+
+
+namespace DynamicProgramming.UnitTests.CanSum
+{
+    public class MemoizedCanSumTests
+    {
+        [Fact]
+        public void MemoizedCanSum_Handles_Zero_TargetSum()
+        {
+            var testee = new MemoizedCanSum();
+            Assert.True(testee.CanSum(0, null));
+        }
+
+        [Fact]
+        public void MemoizedCanSum_Handles_Negative_TargetSum()
+        {
+            var testee = new MemoizedCanSum();
+            Assert.False(testee.CanSum(-1, null));
+        }
+    }
+}
