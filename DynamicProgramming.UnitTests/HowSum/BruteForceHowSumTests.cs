9cc28d4c81a7f45b5cefaaf1a2d769da96ca2f50
Juha Kaarlas <11892788+juhakaarlas@users.noreply.github.com>
2021-06-04 17:29:39 +0200

    HowSum implementation (#2)
    * Rename Fibonacci folder for consistency.
    
    * Group all sum examples under one project.
    
    * Fix xmldoc comments.
    
    * Reformat xmldoc comments
    
    * Basic HowSum implementation.
    
    * Extract interface as usual.
    
    * Clarify the deviation in time complexity in the documention.
    
    * Optimized HowSum

---
+++ DynamicProgramming.UnitTests/HowSum/BruteForceHowSumTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace DynamicProgramming.SumProblems.HowSum.Tests
+{
+    /// <summary>
+    /// Test dataset from <see href="https://youtu.be/oBt53YbR9Kk?t=6181">freeCodeCamp.org</see>
+    /// </summary>
+    public class BruteForceHowSumTests
+    {
+        [Theory]
+        [InlineData(7, new int[] { 2, 4 })]
+        public void HowSum_Returns_Null_When_No_Solution_Exists(int targetSum, int[] numbers)
+        {
+            var testee = new BruteForceHowSum();
+
+            Assert.Null(testee.HowSum(targetSum, numbers));
+        }
+
+        [Theory]
+        [InlineData(7, new int[] { 2, 3 }, new int[] { 3, 2, 2 })]
+        [InlineData(7, new int[] { 5, 3, 4, 7 }, new int[] { 4, 3 })]
+        [InlineData(8, new int[] { 2, 3, 5 }, new int[] { 2, 2, 2, 2 })]
+        public void HowSum_Calculates_Examples_Correctly(int targetSum, int[] numbers, int[] expected)
+        {
+            var expectedResult = new List<int>(expected);
+            var testee = new BruteForceHowSum();
+
+            Assert.Equal(expectedResult, testee.HowSum(targetSum, numbers));
+        }
+    }
+}
