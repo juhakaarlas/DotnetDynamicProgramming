d61dd6046d71e97d0bf2a7e2e86bc00519c093b7
Juha Kaarlas <11892788+juhakaarlas@users.noreply.github.com>
2021-06-04 15:09:55 +0200

    Two different implementations for CanSum (#1)
    * Add time and space complexity info.
    
    * Memoized CanSum implementation with the first test cases.
    
    * Fix flipped test case names.
    
    * Add slightly more ambitious test cases.

---
+++ DynamicProgramming.UnitTests/CanSum/MemoizedCanSumTests.cs
@@ -0,0 +1,42 @@
+﻿using DynamicProgramming.CanSum;
+using Xunit;
+
+
+namespace DynamicProgramming.UnitTests.CanSum
+{
+    public class MemoizedCanSumTests
+    {
+        [Fact]
+        public void MemoizedCanSum_Handles_Zero_TargetSum()
+        {
+            var testee = new MemoizedCanSum();
+            Assert.True(testee.CanSum(0, null));
+        }
+
+        [Fact]
+        public void MemoizedCanSum_Handles_Negative_TargetSum()
+        {
+            var testee = new MemoizedCanSum();
+            Assert.False(testee.CanSum(-1, null));
+        }
+
+        [Theory]
+        [InlineData(7, new int[] { 2, 4 })]
+        [InlineData(300, new int[] { 7, 14 })]
+        public void MemoizedCanSum_Calculates_False_Examples_Correctly(long targetSum, int[] numbers)
+        {
+            var testee = new MemoizedCanSum();
+            Assert.False(testee.CanSum(targetSum, numbers));
+        }
+
+        [Theory]
+        [InlineData(7, new int[] { 2, 3 })]
+        [InlineData(8, new int[] { 2, 3, 5 })]
+        [InlineData(7, new int[] { 3, 4, 5, 7 })]
+        public void MemoizedCanSum_Calculates_True_Examples_Correctly(long targetSum, int[] numbers)
+        {
+            var testee = new MemoizedCanSum();
+            Assert.True(testee.CanSum(targetSum, numbers));
+        }
+    }
+}
