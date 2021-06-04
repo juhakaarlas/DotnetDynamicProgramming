b984543e3a97d9f77889f1cffa51a88def3b3882
Juha Kaarlas <juhakaarlas@users.noreply.github.com>
2021-06-04 17:29:39 +0200

    HowSum implementation (#2)
    * Rename Fibonacci folder for consistency.
    
    * Group all sum examples under one project.
    
    * Fix xmldoc comments.
    
    * Reformat xmldoc comments
    
    * Basic HowSum implementation.
    
    * Extract interface as usual.
    
    * Clarify the deviation in time complexity in the documention.
    
    * Optimized HowSum

--- DynamicProgramming.GridTraveler/BruteForceGridTraveler.cs
+++ DynamicProgramming.GridTraveler/BruteForceGridTraveler.cs
@@ -1,21 +1,22 @@
 ﻿namespace DynamicProgramming.GridTraveler
 {
     /// <summary>
+    /// Problem Statement (from freeCodeCamp.org)
     /// <para>
-    /// Problem Statement:
-    /// 
     /// Say that you are a traveler on a 2D grid. You begin in the top-left corner
     /// and your goal is to travel to the bottom-right corner. 
     /// You may only move down or right.
-    /// 
+    /// </para>
+    /// <para>
     /// In how many ways can you travel to the goal on a grid with dimensions w * h?
-    /// 
-    /// Check the <see href="https://www.youtube.com/watch?v=oBt53YbR9Kk&t=2319s">video from freeCodeCamp.org</see>
+    /// </para>
+    /// <para>
+    /// Check the <see href="https://youtu.be/oBt53YbR9Kk?t=2319s">video from freeCodeCamp.org</see>
     /// for a thorough explanation.
     /// </para>
     /// <para>
-    /// This class implements a basic brute force grid traveler algorithm.
-    /// Completes in O(2^(w+h)) time and O(w+h) space.
+    /// This class implements a basic brute force grid traveler algorithm 
+    /// in O(2^(w+h)) time and O(w+h) space.
     /// </para>
     /// </summary>
     public class BruteForceGridTraveler : IGridTraveler
---
+++ DynamicProgramming.SumProblems/HowSum/IHowSum.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace DynamicProgramming.SumProblems.HowSum
+{
+    public interface IHowSum
+    {
+        /// <summary>
+        /// Given an array of <c>numbers</c> and a <c>targetSum</c>, 
+        /// calculates a combination of <c>numbers</c> which add up to 
+        /// exactly <c>targetSum</c>.
+        /// </summary>
+        /// <param name="targetSum">The target sum</param>
+        /// <param name="numbers">The numbers to used in addition.</param>
+        /// <returns>
+        /// A list containing the combination, if one was found. Otherwise <c>null</c>
+        /// </returns>
+        List<int> HowSum(long targetSum, int[] numbers);
+    }
+}
