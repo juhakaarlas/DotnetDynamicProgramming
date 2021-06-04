99256a069f727638449a9f1a1fb3acebc00d4ff0
Juha Kaarlas <juha.kaarlas@gmail.com>
2021-06-04 14:31:49 +0200

    Add slightly more ambitious test cases.

--- DynamicProgramming.UnitTests/CanSum/MemoizedCanSumTests.cs
+++ DynamicProgramming.UnitTests/CanSum/MemoizedCanSumTests.cs
@@ -19,5 +19,24 @@ namespace DynamicProgramming.UnitTests.CanSum
             var testee = new MemoizedCanSum();
             Assert.False(testee.CanSum(-1, null));
         }
+
+        [Theory]
+        [InlineData(7, new int[] { 2, 4 })]
+        [InlineData(300, new int[] { 7, 14 })]
+        public void MemoizedCanSum_Calculates_False_Examples_Correctly(long targetSum, int[] numbers)
+        {
+            var testee = new MemoizedCanSum();
+            Assert.False(testee.CanSum(targetSum, numbers));
+        }
+
+        [Theory]
+        [InlineData(7, new int[] { 2, 3 })]
+        [InlineData(8, new int[] { 2, 3, 5 })]
+        [InlineData(7, new int[] { 3, 4, 5, 7 })]
+        public void MemoizedCanSum_Calculates_True_Examples_Correctly(long targetSum, int[] numbers)
+        {
+            var testee = new MemoizedCanSum();
+            Assert.True(testee.CanSum(targetSum, numbers));
+        }
     }
 }
