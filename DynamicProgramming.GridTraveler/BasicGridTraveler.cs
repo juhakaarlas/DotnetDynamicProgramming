a92585a446fd6b279cbed1a9e25931724c90281a
Juha Kaarlas <juha.kaarlas@gmail.com>
2021-06-04 11:07:36 +0200

    Add GridTraveler problem and reorganize tests.

---
+++ DynamicProgramming.GridTraveler/BasicGridTraveler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DynamicProgramming.GridTraveler
+{
+    /// <summary>
+    /// Implements a basic brute force grid traveler algorithm.
+    /// Completes in O(2^(m+n)) time.
+    /// </summary>
+    public class BasicGridTraveler : IGridTraveler
+    {
+        /// <inheritdoc/>
+        public long GridTraveler(long m, long n)
+        {
+            //Implement the base cases
+            if (m == 1 && n == 1) return 1;
+            if (m == 0 || n == 0) return 0;
+
+            return GridTraveler(m-1, n) + GridTraveler(m, n-1);
+        }
+    }
+}
---
+++ DynamicProgramming.GridTraveler/IGridTraveler.cs
@@ -0,0 +1,16 @@
+﻿namespace DynamicProgramming.GridTraveler
+{
+    interface IGridTraveler
+    {
+        /// <summary>
+        /// Given an m * n grid, 
+        /// when starting from the top left square, 
+        /// and the allowed movements are one square down or one square right,
+        /// returns the number of possible paths to the bottom right corner.
+        /// </summary>
+        /// <param name="m">The width of the grid</param>
+        /// <param name="n">The height of the grid</param>
+        /// <returns>The number or possible paths.</returns>
+        long GridTraveler(long m, long n);
+    }
+}
