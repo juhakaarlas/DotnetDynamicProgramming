31e75aa43575b22d64c86e1fa403c2072d495a23
Juha Kaarlas <juha.kaarlas@gmail.com>
2021-06-04 11:42:24 +0200

    Implemented memoized grid traveler with a common base.

--- DynamicProgramming.GridTraveler/IGridTraveler.cs
+++ DynamicProgramming.GridTraveler/IGridTraveler.cs
@@ -3,14 +3,14 @@
     interface IGridTraveler
     {
         /// <summary>
-        /// Given an m * n grid, 
+        /// Given an w * n grid, 
         /// when starting from the top left square, 
         /// and the allowed movements are one square down or one square right,
         /// returns the number of possible paths to the bottom right corner.
         /// </summary>
-        /// <param name="m">The width of the grid</param>
-        /// <param name="n">The height of the grid</param>
+        /// <param name="w">The width of the grid</param>
+        /// <param name="h">The height of the grid</param>
         /// <returns>The number or possible paths.</returns>
-        long GridTraveler(long m, long n);
+        long GridTraveler(long w, long h);
     }
 }
---
+++ DynamicProgramming.GridTraveler/MemoizedGridTraveler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace DynamicProgramming.GridTraveler
+{
+    public class MemoizedGridTraveler : GridTravelerBase
+    {
+        private Dictionary<string, long> _lookup;
+
+        public MemoizedGridTraveler()
+        {
+            _lookup = new Dictionary<string, long>();
+        }
+        
+        public override long GridTraveler(long w, long h)
+        {
+            long value = BaseCase(w, h);
+            if (value > -1) return value;
+            
+            string key = string.Format("{0},{1}", w, h);
+            
+            if (_lookup.ContainsKey(key))
+            {
+                return _lookup[key];
+            }
+            
+            _lookup[key] = value = GridTraveler(w - 1, h) + GridTraveler(w, h - 1);
+            return value;
+        }
+    }
+}
