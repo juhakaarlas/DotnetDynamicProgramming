64e800fa88081a1442919629ca77f75e3173555e
Juha Kaarlas <11892788+juhakaarlas@users.noreply.github.com>
2021-06-04 10:20:55 +0200

    Extract base class and implemented a memoized Fibonacci calculator.

---
+++ DynamicProgramming.UnitTests/FibonacciBaseTests.cs
@@ -0,0 +1,27 @@
+﻿using DynamicProgramming.Fibonacci;
+using Xunit;
+
+namespace DynamicProgramming.UnitTests
+{
+    public class FibonacciBaseTests : FibonacciBase
+    {
+        [Fact]
+        public void BaseFibonacci_Returns_Base_Cases()
+        {
+            Assert.Equal(1, BaseCase(1));
+            Assert.Equal(1, BaseCase(2));
+        }
+
+        [Fact]
+        public void BaseFibonacci_Handles_Zero()
+        {
+            var testee = new BruteForceFibonacci();
+            Assert.Equal(0, BaseCase(0));
+        }
+
+        public override int Fib(int n)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Fibonacci/MemoizedFibonacci.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming.Fibonacci
+{
+    /// <summary>
+    /// This class implementes the Nth Fibonacci number calculation using memoization.
+    /// It completes in O(n) time and space.
+    /// </summary>
+    public class MemoizedFibonacci : FibonacciBase
+    {
+        private Dictionary<int, int> _lookup;
+
+        public MemoizedFibonacci()
+        {
+            _lookup = new Dictionary<int, int>();
+        }
+        
+        /// <inheritdoc />
+        public override int Fib(int n)
+        {
+            int value = BaseCase(n);
+
+            if (value > -1)
+            {
+                return value;
+            }
+
+            if (_lookup.TryGetValue(n, out value))
+            {
+                return value;
+            }
+
+            value = Fib(n - 2) + Fib(n - 1);
+            _lookup[n] = value;
+            
+            return value;
+        }
+    }
+}
