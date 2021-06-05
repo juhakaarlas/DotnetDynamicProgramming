c324d09c0d87b6d3fe9d85d632c11b4ea9656eb6
Juha Kaarlas <11892788+juhakaarlas@users.noreply.github.com>
2021-06-05 17:30:52 +0200

    Added optimized BestSum implementation.

--- DynamicProgramming.SumProblems/BestSum/MemoizedBestSum.cs
+++ DynamicProgramming.SumProblems/BestSum/MemoizedBestSum.cs
@@ -1,8 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace DynamicProgramming.BestSum
 {
+    /// <summary>
+    /// This class implements the BestSum algorithm in O(n * m^2) time and O(m^2) space
+    /// where m is the <c>targetSum</c> and n is the length of <c>numbers</c>.
+    /// </summary>
     public class MemoizedBestSum : IBestSum
     {
         private Dictionary<int, List<int>> _memo;
@@ -11,12 +14,38 @@ namespace DynamicProgramming.BestSum
         {
             _memo = new Dictionary<int, List<int>>();
         }
+        
+        /// <inheritdoc/>
         public List<int> BestSum(int targetSum, int[] numbers)
         {
-            if (_memo.ContainsKey(targetSum)) return _memo[targetSum];
-            if (targetSum == 0) return new List<int>();
             if (targetSum < 0) return null;
-            return null;
+            if (targetSum == 0) return new List<int>();
+            if (_memo.ContainsKey(targetSum)) return _memo[targetSum];
+            
+            List<int> shortestCombo = null;
+
+            foreach (int num in numbers)
+            {
+                var remainderCombination = BestSum(targetSum - num, numbers);
+
+                if (remainderCombination != null)
+                {
+                    //clone the list - this only works for value types
+                    var combination = new List<int>(remainderCombination);
+                    combination.Add(num);
+
+                    shortestCombo ??= combination;
+
+                    //if the combination is shorter than the current best, update it
+                    if (shortestCombo == null || combination.Count < shortestCombo.Count)
+                    {
+                        shortestCombo = combination;
+                    }
+                }
+            }
+
+            _memo[targetSum] = shortestCombo;
+            return shortestCombo;
         }
     }
 }
