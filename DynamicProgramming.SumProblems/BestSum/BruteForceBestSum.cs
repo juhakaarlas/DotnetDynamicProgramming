2ea0eb1c2b3e2c1c4f3b95d30493cdfb732c16f5
Juha Kaarlas <11892788+juhakaarlas@users.noreply.github.com>
2021-06-05 15:37:47 +0200

    Added BestSum skeleton

---
+++ DynamicProgramming.SumProblems/BestSum/BruteForceBestSum.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace DynamicProgramming.BestSum
+{
+    /// <summary>
+    /// Problem Statement (from freeCodeCamp.org)
+    /// <para>
+    /// Write a method <c>BestSum(int targetSum, int[] numbers)</c> that takes
+    /// in a <c>targetSum</c> and an array of numbers as arguments.
+    /// </para>
+    /// <para>
+    /// The method should return an array containing the <i>shortest</i> combination
+    /// of numbers that add up to exactly the <c>targetSum</c>.
+    /// </para>
+    /// <para>
+    /// If there is a tie for the shortest combination, you may return any
+    /// one of the shortest.
+    /// </para>
+    /// </summary>
+    public class BruteForceBestSum
+    {
+        public List<int> BestSum(int targetSum, int[] numbers)
+        {
+            return null;
+        }
+    }
+}
