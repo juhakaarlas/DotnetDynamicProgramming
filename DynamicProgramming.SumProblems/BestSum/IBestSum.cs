462b2e3e64303c7200a4e5992e07925980593d62
Juha Kaarlas <11892788+juhakaarlas@users.noreply.github.com>
2021-06-06 17:26:31 +0200

    Added BestSum implementation. (#5)
    * Added a better problem statement and credit to source.
    
    * Fix copy paste error of wrong testee.
    Change namespace for consistency.
    
    * Simplify method names and rename namespace for consistency.
    
    * Simplify method names. Move to same namespace with other Fib tests.
    
    * Add consistency check.
    
    * Add consistency check.
    
    * Unify namespace
    
    * Simplify test case names
    
    * Simplify test case naming.
    
    * Added consistency test cases
    
    * Unify namespaces
    
    * Unify namespaces and simplify test case naming.
    
    * Add consistency check.
    
    * Simplify test names
    
    * Added consistency test case.
    
    * Added BestSum skeleton
    
    * Added a brute-ish BestSum implementation.
    
    * Add Big-O info.
    
    * Added base case test.
    
    * Added optimized BestSum implementation.
    
    * Improved documentation.

---
+++ DynamicProgramming.SumProblems/BestSum/IBestSum.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace DynamicProgramming.BestSum
+{
+    public interface IBestSum
+    {
+        /// <summary>
+        /// Given an array of <c>numbers</c> and a <c>targetSum</c>, 
+        /// calculates the <i>shortest</i> possible combination of <c>numbers</c> 
+        /// which add up to exactly <c>targetSum</c>.
+        /// </summary>
+        /// <param name="targetSum">The target sum</param>
+        /// <param name="numbers">The numbers to used in addition.</param>
+        /// <returns>
+        /// A list containing the combination, if one was found. Otherwise <c>null</c>
+        /// </returns>
+        List<int> BestSum(int targetSum, int[] numbers);
+    }
+}
---
+++ DynamicProgramming.UnitTests/BestSum/MemoizedBestSumTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DynamicProgramming.BestSum.Tests
+{
+    public class MemoizedBestSumTests
+    {
+        [Fact]
+        public void Handles_Base_Cases()
+        {
+            var testee = new MemoizedBestSum();
+
+            var shouldBeNull = testee.BestSum(-1, null);
+            var shouldBeZero = testee.BestSum(0, null);
+
+            Assert.Null(shouldBeNull);
+            shouldBeZero.Should().BeEquivalentTo(new List<int>());
+        }
+
+        [Theory]
+        [InlineData(7, new int[] { 5, 3, 4, 7 }, new int[] { 7 })]
+        [InlineData(8, new int[] { 2, 3, 5 }, new int[] { 3, 5 })]
+        [InlineData(8, new int[] { 1, 4, 5 }, new int[] { 4, 4 })]
+        [InlineData(100, new int[] { 1, 2, 5, 25 }, new int[] { 25, 25, 25, 25 })]
+        public void Calculates_Examples_Correctly(int targetSum, int[] numbers, int[] expected)
+        {
+            var testee = new MemoizedBestSum();
+            var expectedList = new List<int>(expected);
+            var actual = testee.BestSum(targetSum, numbers);
+
+            actual.Should().BeEquivalentTo(expectedList);
+        }
+    }
+}
