e3062b50bbe8fa9a8c06834c7e01f736acd03056
Juha Kaarlas <11892788+juhakaarlas@users.noreply.github.com>
2021-06-07 23:18:19 +0200

    Added CountConstruct implementations

---
+++ DynamicProgramming.UnitTests/CountConstruct/BruteForceCountConstructTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace DynamicProgramming.ConstructProblems.Tests
+{
+    /// <summary>
+    /// Test dataset from <see href="https://youtu.be/oBt53YbR9Kk?t=9914">freeCodeCamp.org</see>.
+    /// </summary>
+    public class BruteForceCountConstructTests
+    {
+        [Theory]
+        [InlineData("purple", new string[] { "purp", "p", "ur", "le", "purpl" }, 2)]
+        [InlineData("abcdef", new string[] { "ab", "abc", "cd", "def", "abcd" }, 1)]
+        [InlineData("skateboard", new string[] { "bo", "rd", "ate", "t", "ska", "sk", "boar" }, 0)]
+        [InlineData("enterapotentpot", new string[] { "a", "p", "ent", "enter", "ot", "o", "t" }, 4)]
+        public void Calculates_Example_Cases_Correctly(string target, string[] wordBank, int expected)
+        {
+            var testee = new BruteForceCountConstruct();
+            Assert.Equal(expected, testee.CountConstruct(target, wordBank));
+        }
+    }
+}
