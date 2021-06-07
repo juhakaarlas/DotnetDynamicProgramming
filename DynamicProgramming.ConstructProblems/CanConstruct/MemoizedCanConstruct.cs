a600088b856cb6ec9ce310965d0f221b07794f29
Juha Kaarlas <11892788+juhakaarlas@users.noreply.github.com>
2021-06-07 22:50:43 +0200

    Implemented optimized CanConstruct algo

---
+++ DynamicProgramming.ConstructProblems/CanConstruct/MemoizedCanConstruct.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace DynamicProgramming.ConstructProblems
+{
+    /// <summary>
+    /// /// <para>
+    /// This class implements the CanConstruct algorithm in O(n*m^2) time and O(m^2) space
+    /// where m is the <c>target.Length</c> n <c>wordBank.Length</c>.
+    /// </para>
+    /// </summary>
+    public class MemoizedCanConstruct :ICanConstruct
+    {
+        private Dictionary<string, bool> _memo;
+
+        public MemoizedCanConstruct()
+        {
+            _memo = new Dictionary<string, bool>();
+        }
+
+        public bool CanConstruct(string target, string[] wordBank)
+        {
+            if (_memo.ContainsKey(target)) return _memo[target];
+            if (string.IsNullOrEmpty(target)) return true;
+
+            foreach (string word in wordBank)
+            {
+                if (target.IndexOf(word) == 0)
+                {
+                    string suffix = target.Substring(word.Length);
+                    if (CanConstruct(suffix, wordBank))
+                    {
+                        _memo[target] = true;
+                        return true;
+                    }
+                }
+            }
+
+            _memo[target] = false;
+            return false;
+        }
+
+    }
+}
---
+++ DynamicProgramming.UnitTests/CanConstruct/MemoizedCanConstructTests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+using DynamicProgramming.ConstructProblems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming.ConstructProblems.Tests
+{
+    public class MemoizedCanConstructTests
+    {
+        [Theory]
+        [InlineData("abcdef", new string[] { "ab", "abc", "cd", "def", "abcd" }, true)]
+        [InlineData("skateboard", new string[] { "bo", "rd", "ate", "t", "ska", "sk", "boar" }, false)]
+        [InlineData("enterapotentpot", new string[] { "a", "p", "ent", "enter", "ot", "o", "t" }, true)]
+        [InlineData("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", 
+                    new string[] { "e", "ee", "eee", "eeee", "eeee", "eeeee", "eeeeee" }, false)]
+        public void Calculates_Example_Cases_Correctly(string target, string[] wordBank, bool expected)
+        {
+            var testee = new MemoizedCanConstruct();
+
+            if (expected)
+            {
+                Assert.True(testee.CanConstruct(target, wordBank));
+            }
+            else
+            {
+                Assert.False(testee.CanConstruct(target, wordBank));
+            }
+        }
+    }
+}
