3dfa2db9a4ad8d3734bcfa3882fb497682841911
Juha Kaarlas <11892788+juhakaarlas@users.noreply.github.com>
2021-06-13 17:48:05 +0200

    Added tabulated Fibonacci implementation.

---
+++ DynamicProgramming.Fibonacci/TabulatedFibonacci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DynamicProgramming.Fibonacci
+{
+    /// <summary>
+    /// This class finds the Nth Fibonacci number using tabulation.
+    /// It returns in O(n) time and space.
+    /// The array length and for loop condition are different from those
+    /// in the <see href="https://youtu.be/oBt53YbR9Kk?t=12076">video</see>
+    /// because with the given solution i would actually go out of bounds.
+    /// </summary>
+    public class TabulatedFibonacci : IFibonacci
+    {
+        
+        /// <inheritdoc/>
+        public long Fib(long n)
+        {
+            long[] table = new long[n+2];
+            table[1] = 1;
+
+            for (long i = 0; i < n ; i++)
+            {
+                table[i + 1] += table[i];
+                table[i + 2] += table[i];
+            }
+
+            return table[n];
+        }
+    }
+}
---
+++ DynamicProgramming.UnitTests/TabulatedFibonacciTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using DynamicProgramming.Fibonacci;
+using System;
+
+namespace DynamicProgramming.Fibonacci.Tests
+{
+    public class TabulatedFibonacciTests
+    {
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(3, 2)]
+        [InlineData(6, 8)]
+        [InlineData(7, 13)]
+        [InlineData(8, 21)]
+        [InlineData(50, 12586269025)]
+        public void FibTest(int n, long expected)
+        {
+            var testee = new TabulatedFibonacci();
+            Assert.Equal(expected, testee.Fib(n));
+        }
+    }
+}
